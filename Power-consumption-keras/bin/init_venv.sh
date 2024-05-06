295d49de551d9b85dbff721f23c60f54bdf72cb5
salmantoor <salman@scaleoutsystems.com>
2024-05-06 21:30:04 +0000

    Removeing incompatible  files.

--- Power-consumption-keras/bin/init_venv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Init venv
-python -m venv .power-consumption-keras
-
-# Pip deps
-.power-consumption-keras/bin/pip install --upgrade pip
-.power-consumption-keras/bin/pip install -r requirements.txt
